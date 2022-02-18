a8606ce2254c21a4c53a8fa49ed5009127a35def
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-02-17 22:45:39 -0800

    seeder files

--- mood-project/app/seeds/moodlist.py
+++ mood-project/app/seeds/moodlist.py
@@ -3,39 +3,35 @@ from app.models import db, Moodlist
 
 def seed_moodlist():
     moodlist1= Moodlist(
-        name='Happy1', color='Yellow', userId=1)
+        name='Aggressive', color='Yellow', userId=1)
     moodlist2 = Moodlist(
-        name='Mad1', color='Red', userId=1)
+        name='Confident', color='Red', userId=1)
     moodlist3= Moodlist(
-        name='Sad1', color='Blue', userId=1)
+        name='Gentle', color='Blue', userId=1)
     moodlist4= Moodlist(
-        name='Happy2', color='Orange', userId=1)
+        name='Epic', color='Orange', userId=1)
     moodlist5 = Moodlist(
-        name='Mad2', color='Green', userId=1)
+        name='Dramatic', color='Green', userId=1)
     moodlist6= Moodlist(
-        name='Sad2', color='Pink', userId=1)
+        name='Relaxed', color='Pink', userId=1)
     moodlist7= Moodlist(
-        name='Happy3', color='Silver', userId=1)
+        name='Nostalgic', color='Silver', userId=1)
     moodlist8 = Moodlist(
-        name='Mad3', color='White', userId=1)
+        name='Melodic', color='White', userId=1)
     moodlist9= Moodlist(
-        name='Sad3', color='Purple', userId=1)
+        name='Meditative', color='Purple', userId=1)
     moodlist10= Moodlist(
-        name='Happy3', color='Brown', userId=1)
+        name='Gloomy', color='Brown', userId=1)
     moodlist11 = Moodlist(
-        name='Mad4', color='Balck', userId=1)
+        name='Driving', color='Black', userId=1)
     moodlist12= Moodlist(
-        name='Sad4', color='Tan', userId=1)
+        name='Sad', color='Tan', userId=1)
     moodlist13= Moodlist(
-        name='Happy4', color='Green', userId=1)
-    moodlist13 = Moodlist(
-        name='Mad5', color='Red', userId=1)
-    moodlist14= Moodlist(
-        name='Sad5', color='White', userId=1)
-
-
-    moodlist7=Moodlist(
-        name='Sentimental', color='Grey', userId=1)
+        name='Happy', color='Green', userId=1)
+    moodlist14 = Moodlist(
+        name='Scary', color='Red', userId=1)
+    moodlist15= Moodlist(
+        name='Annoyed', color='White', userId=1)
 
 
 
@@ -54,6 +50,7 @@ def seed_moodlist():
     db.session.add(moodlist12)
     db.session.add(moodlist13)
     db.session.add(moodlist14)
+    db.session.add(moodlist15)
     db.session.commit()
 
 def undo_moodlists():
