d72bbffb183f35b8cfaa1c56aef518f61331b7a5
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-02-19 17:27:13 -0800

    refactored moodlists to work for edit

--- mood-project/react-app/src/components/moodlists/index.js
+++ mood-project/react-app/src/components/moodlists/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Modal } from '../../context/Modal';
 import { Link } from "react-router-dom";
+import { editMoodlist } from "../../store/moodlist";
+
 import {
   loadUserMoods,
   deleteMoodlist,
@@ -16,21 +19,44 @@ const Moodlists = () => {
   const userId = useSelector((state) => state.session.user.id);
   const [showModal, setShowModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
+  const user = useSelector((state) => state.session.user)
+
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
 
 
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    const newmoodlist = {
+      "id": +e.target.id,
+      'name': name,
+      'color': color,
+    }
+    dispatch(editMoodlist(newmoodlist))
+    dispatch(loadUserMoods(userId))
+    setShowEditModal(false)
+  }
 
   useEffect(() => {
     dispatch(loadUserMoods(userId));
   }, [dispatch, showEditModal, showModal]);
 
-  // useEffect(() => {
-  //     dispatch(addMoodList)
-  // }, [dispatch])
+  const handleShowModalData = (e) => {
+    e.preventDefault();
+    moodlists.filter(moodlist => {
+      if (moodlist.id === +e.target.id) {
+        setColor(moodlist.color)
+        setName(moodlist.name)
+      }
+    })
+    setShowEditModal(true)
+  }
 
-  const handleDelete = async (e) => {
+  const handleDelete = (e) => {
     e.preventDefault();
     const id = e.target.id;
-    await dispatch(deleteMoodlist(id));
+    dispatch(deleteMoodlist(id));
   };
   return (
     <>
@@ -40,38 +66,57 @@ const Moodlists = () => {
           <AddFormModal setShowModal={setShowModal} showModal={showModal} />
         </a>
         <div className="moodlistcontent">
-        {moodlists?.map((mood) => (
-          <>
-          <div className="moodboxes">
-            <Link to={`/moodlists/${mood.id}`}>
-              <div className="eachmoodlist" key={mood?.id}>
-                <p id="moodname">{mood?.name}</p>
-              </div>
-            </Link>
-            <div className="buttons-for-moodlists">
-            <button
-              onClick={handleDelete}
-              id={mood?.id}
-              className="moodlistedit-delete"
-              >
-              Delete
-            </button>
+          {moodlists?.map((mood) => (
+            <>
+              <div className="moodboxes">
+                <Link to={`/moodlists/${mood.id}`}>
+                  <div className="eachmoodlist" key={mood?.id}>
+                    <p id="moodname">{mood?.name} <br/> {mood.id}</p>
+                  </div>
+                </Link>
+                <div className="buttons-for-moodlists">
+                  <button
+                    onClick={handleDelete}
+                    id={mood?.id}
+                    className="moodlistedit-delete"
+                  >
+                    Delete
+                  </button>
 
-            <a className="moodlistedit-delete">
-              <EditFormModal
-              
-                mood={mood?.id}
-                setShowEditModal={setShowEditModal}
-                showEditModal={showEditModal}
-                />
-            </a>
-        </div>
+                  <button id={mood.id} className='moodlistedit-delete' onClick={handleShowModalData}>Edit</button>
+                  {showEditModal && (
+                    <Modal onClose={() => setShowEditModal(false)}>
+                      <form className="edit">
+                        <h1 id="loginheader">Edit</h1>
+                        <label>
+                          <input
+                            className="userinput"
+                            placeholder="New Mood"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                        </label>
+                        <label>
+                          <input
+                            className="passwordinput"
+                            placeholder="Color"
+                            type="text"
+                            value={color}
+                            onChange={(e) => setColor(e.target.value)}
+                          />
+                        </label>
+                        <button id={mood.id} className="loginbutton" type="submit" onClick={handleEdit}>Edit</button>
+                      </form>
+                    </Modal>
+                  )}
                 </div>
-          </>
-        ))}
+              </div>
+            </>
+          )).reverse()}
         </div>
       </div>
-      </>
+    </>
   );
 };
 export default Moodlists;
