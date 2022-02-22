3caa93314e893fefffc5a4229bddb1eb773c8c3e
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-02-21 21:48:16 -0800

    updated color menu

--- mood-project/react-app/src/components/AddMood/AddModal.js
+++ mood-project/react-app/src/components/AddMood/AddModal.js
@@ -1,63 +1,88 @@
-import './AddModel.css'
+import "./AddModel.css";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addMoodList} from "../../store/moodlist";
+import { addMoodList } from "../../store/moodlist";
+
 
 // import "./EditForm.css"
 
-function AddForm({ setShowModal, showModal }) {
-    const userId = useSelector(state => state.session.user.id)
-    const dispatch = useDispatch();
-    const [name, setName] = useState("");
-    const [color, setColor] = useState("");
+function AddForm({ setShowModal }) {
+  const userId = useSelector((state) => state.session.user.id);
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [color, setColor] = useState('')
+  const [errors, setErrors] = useState([]);
 
+  useEffect(() => {
+    const errors = [];
+    if (name.length < 1) errors.push("Name field is required");
+    if (name.length > 15)errors.push("Name can't be longer than 15 letters")
+    setErrors(errors);
+  }, [name, color]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newmoodlist = {
-        'name': name,
-        'color': color,
-        "userId": userId
-    }
+      name: name,
+      color: color,
+      userId: userId,
+    };
 
-    await dispatch(addMoodList(newmoodlist))
-    setShowModal(false)
+    await dispatch(addMoodList(newmoodlist));
+    setShowModal(false);
   };
 
   useEffect(() => {
-    dispatch(addMoodList)
-  }, [dispatch, name, color])
+    dispatch(addMoodList);
+  }, [dispatch]);
 
   return (
-
-    <form className="edit" onSubmit={handleSubmit}>
-      {/* <ul className="errors">
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul> */}
+    <form className="model" onSubmit={handleSubmit}>
+      <ul className="errors">
+        {errors.length > 0 &&
+          errors.map((error) => {
+            return <li key={error}>{error}</li>;
+          })}
+      </ul>
 
       <h1 id="loginheader">New mood</h1>
-        <label>
-          <input
-            className="userinput"
-            placeholder="Enter a new mood"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label>
-          <input
-            className="passwordinput"
-            placeholder="Color Theme"
-            type="text"
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-
-          />
-        </label>
-        <button className="loginbutton" type="submit">Add</button>
+      <label>
+        <input
+          className="nameinput"
+          placeholder="Enter a new mood"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <select
+        className="colorinput"
+        value={color}
+        onClick={(e) => setColor(e.target.value)}
+      >
+        <option className="redstyle" value="red">
+          Red
+          </option>
+        <option className="greenstyle" value="green">
+          Green
+          </option>
+        <option className="bluestyle" value="blue">
+          Blue
+        </option>
+        <option className="purplestyle" value="purple">
+          Purple
+        </option>
+        <option className="blackstyle" value="black">
+          Black
+        </option>
+      </select>
+      <button
+        className="loginbutton"
+        type="submit"
+        disabled={errors.length > 0 ? true : false}
+      >
+        Add
+      </button>
     </form>
   );
 }
